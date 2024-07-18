91fae972f495d5b1330416ca1aa5145180760dd4
Waleed Basit Khan <w.basit2@gmail.com>
2024-07-18 13:19:43 +0100

    Initial commit with Breeze

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        User::firstOrCreate(
+            [
+                "id" => 1,
+            ],
+            [
+                "name" => "Adam Smith",
+                "email" => "fake@email.com",
+                "email_verified_at" => now(),
+                "password" => Hash::make("de!etTh1s"),
+                // 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                "remember_token" => Str::random(10),
+            ]
+        );
+    }
+}
